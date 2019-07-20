690fe92ed88ff7bc2faa60465b54d6bca57c9950
sayantanimondal <sayantanimo@gmail.com>
2019-07-20 16:10:06 +0530

    removing axios call from reducer as this should be a pure function

--- src/login/reducer/loginReducer.ts
+++ src/login/reducer/loginReducer.ts
@@ -1,5 +1,4 @@
 import {Profile} from '../model/Profile';
-import axios from 'axios';
 
 const defaultProfile: Profile = {
     first_name: '',
@@ -11,16 +10,6 @@ export function reduceLoginSuccess(p: Profile = defaultProfile, action: any) {
     switch (action.type) {
 		case 'LMS_LOGIN_ACTION':
 			return {...defaultProfile, email: 'supras@email.com'};
-		
-			// make an ajax call to fetch the details from the database and then set the profile accordingly
-			/*axios.get('http://localhost:9000/employees/supra')
-             .then( (response: any) => {
-				 console.log(response);
-				 return response.data;
-			})
-			 .catch((error: any) => {
-				 console.log('error occured');
-			 });*/
         default:
             return p;
     }
